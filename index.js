f451daaedfe89888d46b61d8ec8e3f4efa3a95fd
Luca Gandini <luca.gandini88@gmail.com>
2022-03-23 17:59:20 +0100

    Utility that return qr code image stream

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 const QRCode = require("qrcode");
+const fs = require("fs");
 
-QRCode.toFile(
-  "filename.png",
-  "https://www.npmjs.com/package/qrcode#qr-code-options",
-  {
-      type: 'png',
-  },
-  function (err) {
-    if (err) throw err;
-    console.log("done");
-  }
-);
+const createImageFromText = (text, outputPath) => {
+  return new Promise((resolve, reject) => {
+    QRCode.toFile(
+      outputPath,
+      text,
+      {
+        type: "png",
+      },
+      (err) => {
+        if (err) {
+          reject(err);
+        }
+        console.log("done");
+        let readStream = fs.createReadStream(outputPath);
+        resolve(readStream);
+      }
+    );
+  });
+};
+
+const src = './qr.png';
+
+const createStreamFromText = (text) => {
+  return new Promise((resolve, reject) => {
+    const stream = fs.createWriteStream(src);
+    QRCode.toFileStream(
+      stream,
+      text,
+      {
+        type: "png",
+      },
+      (err) => {
+        if (err) {
+          reject(err);
+        }
+        console.log("done");
+        resolve(stream);
+      }
+    );
+  });
+};
+
+module.exports = {
+  createImageFromText: createImageFromText,
+  createStreamFromText: createStreamFromText
+};
