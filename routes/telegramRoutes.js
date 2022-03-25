baccfd583829b9b545286572a681bd521d4cc14b
Luca Gandini <luca.gandini88@gmail.com>
2022-03-25 19:12:00 +0100

    image only

--- routes/telegramRoutes.js
+++ routes/telegramRoutes.js
@@ -9,48 +9,39 @@ const handleNewMessage = async (req, res, _next) => {
   console.log(req.body);
   const userMessage = req.body.message;
 
-  const body = {
-    chat_id: req.body.message.chat.id,
-    method: "sendMessage",
-    text: userMessage.text,
-  };
-
-  await fetch(
-    `${telegramAPIBaseUrl}${process.env.TELEGRAM_TOKEN}/sendMessage`,
-    {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: { "Content-Type": "application/json" },
-    }
-  );
-
-  qrcode.createImageFromTextSync(userMessage.text, async (error, readStream) => {
-    if (!error) {
-      let form = new FormData();
-      form.append("photo", readStream);
+  qrcode.createImageFromTextSync(
+    userMessage.text,
+    async (error, readStream) => {
+      if (!error) {
+        let form = new FormData();
+        form.append("photo", readStream);
+        await fetch(
+          `${telegramAPIBaseUrl}${process.env.TELEGRAM_TOKEN}/sendPhoto?chat_id=${req.body.message.chat.id}`,
+          {
+            method: "POST",
+            body: form,
+          }
+        );
+        res.end();
+      }
+      const body = {
+        chat_id: req.body.message.chat.id,
+        method: "sendMessage",
+        text: "Something went wrong :\ Please try again later.",
+      };
+    
       await fetch(
-        `${telegramAPIBaseUrl}${process.env.TELEGRAM_TOKEN}/sendPhoto?chat_id=${req.body.message.chat.id}`,
+        `${telegramAPIBaseUrl}${process.env.TELEGRAM_TOKEN}/sendMessage`,
         {
           method: "POST",
-          body: form,
+          body: JSON.stringify(body),
+          headers: { "Content-Type": "application/json" },
         }
       );
+
       res.end();
     }
-    res.end();
-  });
-  // const stream = await qrcode.createStreamFromText(userMessage.text);
-
-  // form.append("photo", stream);
-  // await fetch(
-  //   `${telegramAPIBaseUrl}${process.env.TELEGRAM_TOKEN}/sendPhoto?chat_id=${req.body.message.chat.id}`,
-  //   {
-  //     method: "POST",
-  //     body: form,
-  //   }
-  // );
-
-  
+  );
 };
 
 router.post("/", handleNewMessage);
