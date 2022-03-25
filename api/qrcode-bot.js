5ad04059b115b9e3476a63126eb2b38019e64677
Luca Gandini <luca.gandini88@gmail.com>
2022-03-25 12:07:14 +0100

    Logs

--- api/qrcode-bot.js
+++ api/qrcode-bot.js
@@ -1,9 +1,10 @@
-import qrcodTest from "../index";
+// import qrcodTest from "../index";
 
 export default function handler(request, response) {
-  const { message } = request.body;
+  // const { message } = request.body;
   // message.chat.id
   // message.text
+  console.log(request.query);
 
   response.status(200).json({
     body: request.body,
