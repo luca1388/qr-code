c1e0fc974ea3157590c0a96e0fba6f736f78e0cf
Luca Gandini <luca.gandini88@gmail.com>
2022-03-25 18:09:43 +0100

    Removed await

--- api/qrcode.ts
+++ api/qrcode.ts
@@ -8,21 +8,29 @@ export default async function handler(request, response) {
   // message.text
   console.log(message);
 
-  sendMessage(message.chat.id, "test!");
-  response.status(200);
+  // sendMessage(message.chat.id, "test!");
+  response.status(200).json({
+    body: {
+      chat_id: message.chat.id,
+      method: "sendMessage",
+      text: "test!",
+    },
+    query: request.query,
+    cookies: request.cookies,
+  });;
   
 }
 
-const sendMessage = async (chatId, text) => {
-  const body = {
-    chat_id: chatId,
-    method: "sendMessage",
-    text: text,
-  };
+// const sendMessage = async (chatId, text) => {
+//   const body = {
+//     chat_id: chatId,
+//     method: "sendMessage",
+//     text: text,
+//   };
 
-  fetch(`${telegramAPIBaseUrl}${process.env.TELEGRAM_TOKEN}/sendMessage`, {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: { "Content-Type": "application/json" },
-  });
-};
+//   fetch(`${telegramAPIBaseUrl}${process.env.TELEGRAM_TOKEN}/sendMessage`, {
+//     method: "POST",
+//     body: JSON.stringify(body),
+//     headers: { "Content-Type": "application/json" },
+//   });
+// };
