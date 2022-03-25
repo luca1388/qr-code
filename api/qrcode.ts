8c35a1f0ddc4f05ccbd915af353fccc26170ee03
Luca Gandini <luca.gandini88@gmail.com>
2022-03-25 12:27:40 +0100

    Added response

--- api/qrcode.ts
+++ api/qrcode.ts
@@ -1,16 +1,37 @@
 // import qrcodTest from "../index";
+import fetch from "node-fetch";
+const telegramAPIBaseUrl = "https://api.telegram.org/bot";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
   const { message } = request.body;
   // message.chat.id
   // message.text
   console.log(message);
 
-  response.status(200).json({
-    body: {
-      chat_id: message.chat.id,
-      method: 'sendMessage',
-      text: 'test!'
-    },
-  });
+  await sendMessage(message.chat.id, "test!");
+
+  response.status(200);
 }
+
+const sendMessage = async (chatId, text) => {
+  const body = {
+    body: {
+      chat_id: chatId,
+      method: "sendMessage",
+      text: text,
+    },
+  };
+
+  const res = await fetch(
+    `${telegramAPIBaseUrl}/${process.env.TELEGRAM_TOKEN}/sendMessage`,
+    {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  console.log(res.status);
+  const data = await res.json();
+  console.log(data);
+  return data;
+};
