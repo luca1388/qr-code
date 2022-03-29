2d59fad94217531dab836b3fa6106249b9d17364
Luca Gandini <luca.gandini88@gmail.com>
2022-03-30 01:10:28 +0200

    Logs

--- routes/telegramRoutes.js
+++ routes/telegramRoutes.js
@@ -7,7 +7,8 @@ const router = express.Router();
 const FREE_COUNT_THRESHOLD = 5;
 
 const handleNewMessage = async (req, res, _next) => {
-  console.log(req.body);
+  console.log("New mwessage received");
+
   const { message } = req.body;
   const { from, chat } = message;
   const chatId = chat.id;
@@ -16,6 +17,8 @@ const handleNewMessage = async (req, res, _next) => {
   if (message.text[0] === "/") {
     const command = message.text.toLowerCase().split("/")[1];
     // debug message
+    console.log(`Command detected: ${command}`);
+    // TODO: remove
     sendMessage({
       chat_id: chatId,
       method: "sendMessage",
@@ -38,26 +41,26 @@ const handleNewMessage = async (req, res, _next) => {
       default:
       // Send not understand message
     }
-    res.end();
-    return;
+    closeRequest(res);
   }
 
   const userFound = await getDBUser(chatId);
-  console.log(userFound);
   if (userFound) {
-    console.log("user found!");
+    console.log("User already provisioned");
     if (userFound.count < FREE_COUNT_THRESHOLD) {
+      console.log("Updating qr counts for user");
       patchUser(userFound.id, { count: userFound.count + 1 });
     } else {
+      console.log("User exceeds free count limit");
       if (!userFound.premium) {
+        console.log("Sending message for payment");
         // send message for payment
         sendMessage({
           chat_id: chatId,
           method: "sendMessage",
           text: `Hai raggiunto il limite di ${FREE_COUNT_THRESHOLD} QR code generati gratuitamente. Per continuare ad utilizzare il servizio SENZA LIMITI effettua il pagamento del piano premium che ti garantisce un numero illimitato di QR code generati.`,
         });
-        res.end();
-        return;
+        closeRequest(res);
       }
     }
   } else {
@@ -70,19 +73,29 @@ const handleNewMessage = async (req, res, _next) => {
       premium: false,
     });
   }
-
+  console.log("Start creating QR image");
   qrcode.createImageFromTextSync(message.text, async (error, readStream) => {
     if (!error) {
+      console.log("Image created: sending to user ...");
       sendPhoto(chatId, readStream);
     } else {
+      console.error(
+        "Error while creating image: sending error message to user ..."
+      );
       sendMessage({
         chat_id: req.body.message.chat.id,
         method: "sendMessage",
-        text: "Something went wrong : Please try again later.",
+        text: "Something went wrong, please try again later.",
       });
     }
-    res.end();
+    closeRequest(res);
   });
+};
+
+const closeRequest = (res) => {
+  console.log("Request closed");
+  res.end();
+  return;
 };
 
 router.post("/", handleNewMessage);
